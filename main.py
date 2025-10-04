4388b2a9dfdd4969611d88524a4bf5bab7752612
ts511kev <taku_yes_2007@yahoo.co.jp>
2025-10-04 19:52:01 +0900

    微調整

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from musicwait import Music, MusicWait
 from tqdm import tqdm
 import time
 
-with MusicWait(music=Music.OH_SUSANNA, lyrics=False):
-    for _ in tqdm(range(100)):
+with MusicWait(music=Music.OH_SUSANNA, bpm=8, lyrics=True):
+    for _ in range(100):
         time.sleep(0.3)
 
 print("Done!")
